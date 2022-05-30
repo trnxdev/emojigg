a471e184ea48667d2dc2fac060ebcaab56c2cdb0
Alex <me@djoh.xyz>
2022-05-30 20:38:48 +0300

    chore: remove useless promises...
    add invalid arg type error

--- lib/emoji.ts
+++ lib/emoji.ts
@@ -2,21 +2,18 @@ import axios from 'axios';
 import { EmojiGetArg, EmojiGgStats, EmojiResult } from './@types';
 
 class Exports {
-    constructor() {}
+    constructor() { }
 
-    static get(arg: EmojiGetArg): Promise<EmojiResult | EmojiResult[]> {
-        return new Promise(async (resolve) => {
-            if(typeof arg === 'number')
-                resolve(<EmojiResult>(await axios.get(`https://emoji.gg/api/`)).data.find((emoji: EmojiResult) => emoji.id === arg));
-            else if(typeof arg === 'string')
-                resolve(<EmojiResult[]>(await axios.get(`https://emoji.gg/api/`)).data.filter((g: EmojiResult) => g.title.includes(arg)));
-        })
+    static async get(arg: EmojiGetArg): Promise<EmojiResult | EmojiResult[]> {
+        if (typeof arg === 'number')
+            return (await axios.get(`https://emoji.gg/api/`)).data.find((emoji: EmojiResult) => emoji.id === arg);
+        else if (typeof arg === 'string')
+            return (await axios.get(`https://emoji.gg/api/`)).data.filter((g: EmojiResult) => g.title.includes(arg));
+        else throw new RangeError("Invalid argument type: " + typeof arg);
     }
 
-    static stats(): Promise<EmojiGgStats> {
-        return new Promise(async (resolve) => {
-            resolve((await axios.get(`https://emoji.gg/api/stats`)).data);
-        })
+    static async stats(): Promise<EmojiGgStats> {
+        return (await axios.get(`https://emoji.gg/api/stats`)).data;
     }
 }
 
